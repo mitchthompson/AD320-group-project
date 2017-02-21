35a487a046c4791c04f048588644bbf01477c48e
ahand <spottydj@gmail.com>
2017-02-20 20:45:15 -0800

    Added Book class

---
+++ Book.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ahand
+ * Date: 2/20/17
+ * Time: 8:36 PM
+ */
+class Book
+{
+    private $author,$title,$ISBN,$year,$thumbnail;
+
+    /**
+     * Book constructor.
+     * @param $author
+     * @param $title
+     * @param $ISBN
+     * @param $year
+     * @param $thumbnail
+     */
+    public function __construct($author, $title, $ISBN, $year, $thumbnail)
+    {
+        $this->author = $author;
+        $this->title = $title;
+        $this->ISBN = $ISBN;
+        $this->year = $year;
+        $this->thumbnail = $thumbnail;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getISBN()
+    {
+        return $this->ISBN;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getThumbnail()
+    {
+        return $this->thumbnail;
+    }
+
+    public function getElement(){
+        return <<<BOOK
+
+            <div class="book">
+            <img src="$this->thumbnail"/>
+            <h3>TITLE:  $this->title</h3>
+            <h4>AUTHOR: $this->author</h4>
+            <h4>ISBN:   $this->ISBN</h4>
+            </div>
+BOOK;
+    }
+
+
+}
