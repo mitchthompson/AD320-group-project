e57ee2f0546166c7825c2b386de6dfa50217babf
ahand <spottydj@gmail.com>
2017-02-28 22:06:17 -0800

    changes to Book class.

--- Book.php
+++ Book.php
@@ -8,81 +8,64 @@
  */
 class Book
 {
-    private $author,$title,$ISBN,$year,$thumbnail;
 
-    /**
-     * Book constructor.
-     * @param $author
-     * @param $title
-     * @param $ISBN
-     * @param $year
-     * @param $thumbnail
-     */
-    public function __construct($author, $title, $ISBN, $year, $thumbnail)
-    {
-        $this->author = $author;
-        $this->title = $title;
-        $this->ISBN = $ISBN;
-        $this->year = $year;
-        $this->thumbnail = $thumbnail;
-    }
+    //sample
+    private static $select = <<<'STMT'
 
-    public static function retrieveBook(){
-        $stmt = $pdo->prepare('SELECT isbn, author, title, year, thumbnail FROM books WHERE id=?');
-        $stmt->execute([$id]);
-        return $stmt->fetchObject(__CLASS__);
+    SELECT b.isbn_10
+  , b.title
+  , b.publish_date
+  , b.thumbnail_url
+  , a.author_first_name
+  , a.author_last_name
+
+    FROM book b
+    INNER JOIN author_book ab
+        on b.isbn_10 = ab.isbn_10
+    INNER JOIN author a
+        on ab.author_id = a.author_id;
+    
+STMT;
+
+
+
+    protected static $isbn_10, $title, $publish_date, $thumbnail_url, $author_first, $author_last;
+
+
+    private function __construct(){}
+
+
+
+    private static function retrieveBook($isbn){
+        $dbPDO = new dbPDO();
+        $stmt = $dbPDO->prepare(self::$select);
+        $stmt->execute([$isbn]);
+        $result =  $stmt->fetchObject(__CLASS__);
+
+        //close conn
+        $dbPDO = null;
+        $stmt = null;
+
+
 }
 
-    /**
-     * @return mixed
-     */
-    public function getAuthor()
-    {
-        return $this->author;
-    }
 
-    /**
-     * @return mixed
-     */
-    public function getTitle()
-    {
-        return $this->title;
-    }
+    public static function getBookData($isbn){
 
-    /**
-     * @return mixed
-     */
-    public function getISBN()
-    {
-        return $this->ISBN;
-    }
+        self::retrieveBook($isbn);
 
-    /**
-     * @return mixed
-     */
-    public function getYear()
-    {
-        return $this->year;
-    }
+        $book = '<div class="book">';
 
-    /**
-     * @return mixed
-     */
-    public function getThumbnail()
-    {
-        return $this->thumbnail;
-    }
+        if(self::$isbn_10){
+            $book .= "<img src='" . self::$thumbnail_url . "'>'";
+            $book .= "<h3>TITLE:".self::$title."</h3>";
+            $book .= "<h4>DATE:".self::$publish_date."</h4>";
+            $book .= "<h4>AUTHOR:".self::$author_last.", ".self::$author_last."</h4>";
+            $book .= "<h4>ISBN:".self::$isbn_10."</h4>";
+            $book .= "</div>";
+        }
 
-    public function getElement(){
-        return <<<BOOK
-
-            <div class="book">
-            <img src="$this->thumbnail"/>
-            <h3>TITLE:  $this->title</h3>
-            <h4>AUTHOR: $this->author</h4>
-            <h4>ISBN:   $this->ISBN</h4>
-            </div>
-BOOK;
+        return $book;
     }
 
 
