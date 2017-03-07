ed08131de607a15656a0a3278589be2c1800daef
kazerwit <numberalternative@gmail.com>
2017-03-07 15:29:34 -0800

    Update register.php

--- register.php
+++ register.php
@@ -1,22 +1,46 @@
 <?php include 'includes/header.php'?>
+<?php 
+//set default values for variables
+if (!isset($first_name)) {$first_name = ''; }
+if (!isset($last_name)) {$last_name = ''; }
+if (!isset($username)) {$username = ''; }
+if (!isset($password)) {$password = ''; }
+if (!isset($email)) {$email = ''; }
+if (!isset($postalcode)) {$postalcode = ''; }
+
+?>
+
 <main>
+	<h1>Please complete all fields to sign up</h1>
     <div class="jumbotron intro">
         <div class="container">
             <div class="row">
                 <div class="col-md-offset-2 col-md-8">
                     <form action="search.php" method="post">
-                            <div class="form-group">
-                                <input class="form-control" type="text" placeholder="Username" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Username'" name="username">
+							<div class="form-group">
+                                <input class="form-control" type="text" placeholder="First Name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'First Name'" name="first_name" value="<?php echo htmlspecialchars($first_name); ?>">
                             </div>
-                            <div class="form-group">
-                                <input class="form-control" type="text" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'" name="password">
+							<br>
+							<div class="form-group">
+                                <input class="form-control" type="text" placeholder="Last Name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Last Name'" name="last_name" value="<?php echo htmlspecialchars($last_name); ?>">
                             </div>
-                            <div class="form-group">
-                                <input class="form-control" type="text" placeholder="Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email'" name="email">
+							<br>
+							<div class="form-group">
+                                <input class="form-control" type="text" placeholder="Username" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Username'" name="username" value="<?php echo htmlspecialchars($username); ?>">
                             </div>
+							<br>
                             <div class="form-group">
-                                <input class="form-control" type="text" placeholder="Postal Code" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Postal Code'" name="postalcode">
+                                <input class="form-control" type="text" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'" name="password" value="<?php echo htmlspecialchars($password); ?>">
                             </div>
+							<br>
+                            <div class="form-group">
+                                <input class="form-control" type="text" placeholder="Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email'" name="email" value="<?php echo htmlspecialchars($email); ?>">
+                            </div>
+							<br>
+                            <div class="form-group">
+                                <input class="form-control" type="text" placeholder="Postal Code" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Postal Code'" name="postalcode" value="<?php echo htmlspecialchars($postalcode); ?>">
+                            </div>
+							<br>
                             <div class="form-group">
                                 <input class="btn btn-secondary btn-block" type="submit" value="Register">
                             </div>    
@@ -27,10 +51,6 @@
     </div><!--end jumbrotron intro-->
     
 </main>
-
-
-
-
 </body>
 <?php include 'includes/footer.php' ?>
    
