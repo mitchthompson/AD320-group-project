d1ca3ce758721faaccfb741033953f900bf5c21c
kazerwit <numberalternative@gmail.com>
2017-03-09 00:00:48 -0800

    Update register.php

--- register.php
+++ register.php
@@ -1,52 +1,56 @@
 <?php include 'includes/header.php'?>
 <?php 
+
 //set default values for variables
-if (!isset($first_name)) {$first_name = ''; }
-if (!isset($last_name)) {$last_name = ''; }
-if (!isset($username)) {$username = ''; }
-if (!isset($password)) {$password = ''; }
-if (!isset($email)) {$email = ''; }
-if (!isset($postalcode)) {$postalcode = ''; }
+$first_name = '';
+$last_name = '';
+$username = '';
+$password = '';
+$email = '';
+$city = '';
+$state = '';
+$message = '';
 
 ?>
 
 <main>
-	<h1>Please complete all fields to sign up</h1>
+<h1>Please complete all fields to sign up for BookSwap:</h1>
     <div class="jumbotron intro">
         <div class="container">
             <div class="row">
                 <div class="col-md-offset-2 col-md-8">
-				<?php if (!empty($error_message)) { ?>
-					<p><?php echo htmlspecialchars($error_message); ?></p>
-				<?php } ?>	
                     <form action="registration.php" method="post">
 							<div class="form-group">
-                                <input class="form-control" type="text" placeholder="First Name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'First Name'" name="first_name" value="<?php echo htmlspecialchars($first_name); ?>">
+                                <input class="form-control" type="text" placeholder="First Name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'First Name'" name="first_name" value="<?php if(isset($_POST['first_name'])) echo $_POST['first_name']; ?>">
                             </div>
 							<br>
 							<div class="form-group">
-                                <input class="form-control" type="text" placeholder="Last Name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Last Name'" name="last_name" value="<?php echo htmlspecialchars($last_name); ?>">
+                                <input class="form-control" type="text" placeholder="Last Name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Last Name'" name="last_name" value="<?php if(isset($_POST['last_name'])) echo $_POST['last_name']; ?>">
                             </div>
 							<br>
 							<div class="form-group">
-                                <input class="form-control" type="text" placeholder="Username" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Username'" name="username" value="<?php echo htmlspecialchars($username); ?>">
+                                <input class="form-control" type="text" placeholder="Username" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Username'" name="username" value="<?php if(isset($_POST['username'])) echo $_POST['username']; ?>">
                             </div>
 							<br>
                             <div class="form-group">
-                                <input class="form-control" type="text" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'" name="password" value="<?php echo htmlspecialchars($password); ?>">
+                                <input class="form-control" type="text" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'" name="password" value="<?php if(isset($_POST['password'])) echo $_POST['password']; ?>">
                             </div>
 							<br>
                             <div class="form-group">
-                                <input class="form-control" type="text" placeholder="Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email'" name="email" value="<?php echo htmlspecialchars($email); ?>">
+                                <input class="form-control" type="text" placeholder="Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email'" name="email" value="<?php if(isset($_POST['email'])) echo $_POST['email']; ?>">
+                            </div>
+							<br>
+							<div class="form-group">
+                                <input class="form-control" type="text" placeholder="City" onfocus="this.placeholder = ''" onblur="this.placeholder = 'City'" name="city" value="<?php if(isset($_POST['city'])) echo $_POST['city']; ?>">
                             </div>
 							<br>
                             <div class="form-group">
-                                <input class="form-control" type="text" placeholder="Postal Code" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Postal Code'" name="postalcode" value="<?php echo htmlspecialchars($postalcode); ?>">
+                                <input class="form-control" type="text" placeholder="State" onfocus="this.placeholder = ''" onblur="this.placeholder = 'State'" name="state" value="<?php if(isset($_POST['state'])) echo $_POST['state']; ?>">
                             </div>
 							<br>
-                            <div class="form-group">
-                                <input class="btn btn-secondary btn-block" type="submit" value="Register">
-                            </div>    
+							<div class="form-group">
+                             <input class="btn btn-secondary btn-block" type="submit" value="Register">
+                            </div>  
                     </form>
                 </div><!--col-md-12-->
             </div>
@@ -56,4 +60,5 @@ if (!isset($postalcode)) {$postalcode = ''; }
 </main>
 </body>
 <?php include 'includes/footer.php' ?>
+
    
