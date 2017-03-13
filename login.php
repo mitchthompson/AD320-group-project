12c90ba3939670e5128102ec5f2abcb70e8f24fa
Ilsa Coleman <imcoleman@users.noreply.github.com>
2017-03-12 18:12:40 -0700

    Update login.php
    Changed db connection to match db connection in book, utilizing db/dbPDO.php.  
    This way only the db_setting.ini file has to be updated with db credentials.
    This required updating mysqli to pdo queries.

--- login.php
+++ login.php
@@ -1,36 +1,30 @@
 <?php 
-//TODO - update DB credentials
+//TODO - update DB credentials on db_settings.ini file
 
 session_start();
 include 'includes/header.php';
+include 'db/dbPDO.php';
 
 //if login form submitted
 if (isset($_POST['submit'])){ 
     
     //connect to database
-    $servername = "localhost";
-    $username = "testuser";
-    $password = "testuserpass";
-    $dbname = "ul";
-    
-    $connection = mysqli_connect($servername, $username, $password, $dbname);
-    if (!$connection){
-        die("Database Connection Failed" . mysqli_error($connection));
-    }
+    $connection = new dbPDO();
     
     //query database with username & password user entered
     $username = $_POST['username'];
     $password = $_POST['password'];
     
     
-    $query = "SELECT * FROM `user` WHERE user_email='$username' and user_password='$password'";
- 
-    $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
-    $row = mysqli_num_rows($result);
+    try {
+    $conn = new dbPDO();
+    $query = $conn->prepare("SELECT * FROM `user` WHERE user_email='$username' and user_password='$password'");
+    $query->execute();
+    $row = $query->rowCount();
     
     //if username/password found
     if ($row == 1){
-        while($row = mysqli_fetch_assoc($result)) {
+        while($row = $query->fetch(PDO::FETCH_ASSOC)) {
             $_SESSION['is_auth'] = true;
             $_SESSION['user_id'] = $row['user_id'];
             $_SESSION['user_first_name'] = $row['user_first_name'];
@@ -85,6 +79,12 @@ if (isset($_POST['submit'])){
     
 </main>';
 
+   }
+        $conn = null;
+        $sth = null;
+
+    } catch (PDOException $p) {
+        echo $p->getMessage();
     }
 }else{
      echo '<main>
