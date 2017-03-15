b3d4f0b317cab4617dd9801924949143cde2d35b
Ilsa Coleman <imcoleman@users.noreply.github.com>
2017-03-14 22:20:49 -0700

    Update search.php
    Added calls to Library to retrieve books searched for along with user information.

--- search.php
+++ search.php
@@ -1,11 +1,31 @@
-<?php include 'includes/header.php'?>
+<?php 
+
+include 'includes/header.php';
+include 'Library.php';
+
+// get isbn from search box on user's page
+if ((isset($_POST['isbn'])) {
+    $isbn = $POST['isbn'];
+}    
+    
+
+?>
+
 <main>
     <div class="jumbotron user">
         <div class="container">
             <div class="row">
-                <div class="col-md-12">
-                    <p>Placeholder Text.</p>
-                </div><!--col-md-12-->
+                <div class="col-md-3">
+                    <ul class="list-inline">
+                        <?php Library::getBook($isbn); ?>
+                    </ul>
+                 </div>
+                 <div class="col-md-3">
+                    <h4 class="text-center">Contact information for users with this book:</h4>
+                    <ul class="list-inline">
+                        <?php Library::getUsersByBook($isbn); ?>
+                    </ul>
+                </div><!--col-md-3-->
             </div><!--row-->
         </div><!--container-->
     </div><!--jumbotron-->
