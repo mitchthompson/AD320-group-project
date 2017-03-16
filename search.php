9e99c3b9380245bd5f299c2074dd8acb7303def8
Ilsa Coleman <imcoleman@users.noreply.github.com>
2017-03-15 22:19:44 -0700

    Update search.php
    Small changes to formatting

--- search.php
+++ search.php
@@ -19,10 +19,17 @@ if ((isset($_POST['isbn'])) {
                     <ul class="list-inline">
                         <?php Library::getBook($isbn); ?>
                     </ul>
-                 </div>
+                </div><!--col-md-3-->
+            </div><!--row-->
+        </div><!--container-->
+    </div><!--jumbotron--> 
+    
+    <div class="jumbotron user">
+        <div class="container">
+            <div class="row">
                  <div class="col-md-3">
-                    <h4 class="text-center">Contact information for users with this book:</h4>
                     <ul class="list-inline">
+                        <h4 class="text-center">Contact information for users with this book:</h4>
                         <?php Library::getUsersByBook($isbn); ?>
                     </ul>
                 </div><!--col-md-3-->
