723a4dbcb932a49b313f37d89ccf0f7ccf56f508
Ilsa Coleman <imcoleman@users.noreply.github.com>
2017-03-19 00:01:10 -0700

    Update BookSwap-ul-MySQL-Create-DB.sql

--- sql/BookSwap-ul-MySQL-Create-DB.sql
+++ sql/BookSwap-ul-MySQL-Create-DB.sql
@@ -17,29 +17,25 @@ USE ul;
 -- create tables
 CREATE TABLE user
 (
-  user_id           INT             NOT NULL 	AUTO_INCREMENT,
-  user_first_name   VARCHAR(50)		  NOT NULL,
+  user_id           INT             NOT NULL 	PRIMARY KEY AUTO_INCREMENT,
+  user_first_name   VARCHAR(50)		NOT NULL,
   user_last_name    VARCHAR(50)     NOT NULL,
   user_city         VARCHAR(20)		  NOT NULL,
   user_state        VARCHAR(2)      NOT NULL,
   user_email		    VARCHAR(50)		  NOT NULL,
   user_password		  VARCHAR(255)		NOT NULL,
-  CONSTRAINT user_pk 
-    PRIMARY KEY (user_id),
   CONSTRAINT user_email_unq
     UNIQUE (user_email)
 );
 
 CREATE TABLE book
 (
-  isbn     		    VARCHAR(13)       NOT NULL,
+  isbn     		    VARCHAR(13)    PRIMARY KEY  NOT NULL,
   title         	VARCHAR(100)	    NOT NULL,
   author          VARCHAR(255)      NOT NULL,
   publishers		  VARCHAR(100)	    NOT NULL,
   publish_date		YEAR			        NOT NULL,
-  thumbnail_url		VARCHAR(255),			
-  CONSTRAINT isbn_pk 
-    PRIMARY KEY (isbn)
+  thumbnail_url		VARCHAR(255)
 );
 
 CREATE TABLE user_owns_book
